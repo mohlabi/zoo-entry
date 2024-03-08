816fc42bb4925eed10e4eadf506b516cc040068a
mohlabi <138671380+mohlabi@users.noreply.github.com>
2024-03-08 01:03:09 -0800

    Add files via upload

---
+++ my file.js
@@ -0,0 +1,25 @@
+function calculatePrice(event) {
+    event.preventDefault();
+    var age = parseInt(document.getElementById('age').value);
+    var price;
+
+    if (age <= 4) {
+        price = 0; // price for babies
+    } else if (age >= 5 && age < 18) {
+        price = 120; // Price for children
+    } else if (age >= 18 && age < 60) {
+        price = 120; // Price for adults
+    } else {
+        price = 0;
+    }
+
+    document.getElementById('result').innerText = "The price of entry is: R" + price;
+    document.getElementById('buyi').style.display = 'block';
+}
+
+function calculateTotal() {
+    const quantity = parseInt(document.getElementById('quantity').value);
+    const price = parseInt(document.getElementById('result').innerText.split('R')[1]);
+    const totalprice = price * quantity;
+    document.getElementById('totalResult').innerText = "Total Price: " + totalprice + " ";
+}
